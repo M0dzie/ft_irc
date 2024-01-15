23bd3dfadead8abb3aafdb55769930ed5417413b
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-15 14:36:18 +0100

    Refactor: change id for fd in class Client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 10:37:02 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/15 14:20:02 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/15 14:31:10 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@ class Client {
 private:
     Client() {}
 
-    int _id;
+    int _fd;
     std::string _nickname;
     std::string _user;
 
 public:
-    Client(int id) : _id(id) {}
+    Client(int fd, std::string const &nickname) : _fd(fd), _nickname(nickname) {}
 
-    void setID(int const &id) {this->_id = id;}
+    void setID(int fd) {this->_fd = fd;}
     void setNickname(std::string const &nickname) {this->_nickname = nickname;}
     void setUser(std::string const &user) {this->_user = user;}
 
-    int const &getID() const {return this->_id;}
+    int const &getFD() const {return this->_fd;}
     std::string const &getNickname() const {return this->_nickname;}
     std::string const &getUser() const {return this->_user;}
 };
