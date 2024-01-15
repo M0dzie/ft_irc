2a190ac2d0134f4ff8274a0a7182c90d8a872e34
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-15 12:29:30 +0100

    Add: Precision with port error and add Warning for port 0-1024

--- sources/parsing/parsing.cpp
+++ sources/parsing/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:08:58 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/09 12:49:09 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/15 12:28:20 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ bool isPortValid(char *argv) {
         if (!std::isdigit(argv[i]))
             return (displayErrorMessage("Port must be a positive number."), false);
     if (std::atoi(argv) > 65535)
-        return (displayErrorMessage("Port available : [1024-65535]"), false);
+        return (displayErrorMessage("Port available : [0-65535]"), false);
+    if (std::atoi(argv) < 1025) {
+        std::cerr << YELLOW << "Warning: " << RESET << "This port might be reserved to a root access." << std::endl;
+        return (true);
+    }
     return true;
 }
