30af289a123403439884734860e348c6e278ac93
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-15 12:38:43 +0100

    Add: bold to warning msg

--- sources/parsing/parsing.cpp
+++ sources/parsing/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:08:58 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/15 12:28:20 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/15 12:30:51 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ bool isPortValid(char *argv) {
     if (std::atoi(argv) > 65535)
         return (displayErrorMessage("Port available : [0-65535]"), false);
     if (std::atoi(argv) < 1025) {
-        std::cerr << YELLOW << "Warning: " << RESET << "This port might be reserved to a root access." << std::endl;
+        std::cerr << YELLOW << BOLD << "Warning: " << RESET << "This port might be reserved to a root access." << std::endl;
         return (true);
     }
     return true;
