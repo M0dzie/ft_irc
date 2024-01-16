0eef317de993a16f4553b0fe45ef06d3a5d108d4
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-16 18:16:25 +0100

    Add: handle signal but need to tests

--- sources/executing/Server.cpp
+++ sources/executing/Server.cpp
@@ -6,14 +6,14 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 10:37:42 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/16 12:48:57 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/16 17:12:00 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/Server.hpp"
 
-/* Thus, if you try to read/recv or write/send in any file descriptor
-without using poll() (or equivalent), your grade will be 0. */
+// non-member attribute to handle signal with sigaction
+bool Server::interrupt = false;
 
 void Server::sendMessage(int clientFd, std::string msg) {
     msg += "\r\n";
@@ -33,9 +33,9 @@ Server::Server(int port) {
 		buffer[i] = '\0';
 
     this->initDataAndServer(port);
+    this->handlingSignal();
 
-
-    while (this->_interrupt == false) {
+    while (Server::interrupt == false) {
         poll(this->_fds, this->_nbClient + 1, -1);
         
         // handling new clients
@@ -44,14 +44,14 @@ Server::Server(int port) {
                 this->_nbClient += 1;
                 if ((this->_fds[this->_nbClient].fd = accept(this->_fds[0].fd, (struct sockaddr *)&this->_address, &this->_addrLen)) < 0 ) {
                     displayErrorMessage("accept() failed.");
-                    this->_interrupt = true;
+                    Server::interrupt = true;
                 }
                 this->_fds[this->_nbClient].events = POLLIN;
                 this->_fds[this->_nbClient].revents = 0;
                 this->_clientList.insert(std::pair<int, Client *>(this->_fds[this->_nbClient].fd, new Client(this->_fds[this->_nbClient].fd, "undefined")));
             } else { // There is no places left
                 displayErrorMessage("The number of client available is full.");
-                this->_interrupt = true;
+                Server::interrupt = true;
             }
         }
         
@@ -60,7 +60,7 @@ Server::Server(int port) {
             if (this->_fds[i].revents & POLLIN) { // there is data ready to recv()
                 if (recv(this->_fds[i].fd, &buffer, bufferSize, 0) == 0) {
                     displayErrorMessage("recv() failed.");
-                    this->_interrupt = true;
+                    Server::interrupt = true;
                 }
                 tmpSentence.append(buffer);
                 std::size_t indexEnd = tmpSentence.find("\r\n");
@@ -82,7 +82,6 @@ Server::Server(int port) {
 void Server::initDataAndServer(int port) {
     this->_opt = 1;
     this->_nbClient = 0;
-    this->_interrupt = false;
     this->_addrLen = sizeof(this->_address);
     this->_address.sin_family = AF_INET;
     this->_address.sin_addr.s_addr = INADDR_ANY;
@@ -106,4 +105,23 @@ void Server::initDataAndServer(int port) {
     if (listen(this->_fds[0].fd, 1) < 0)
         throw(Server::ServerError(ERROR "listen() failed."));
 
+}
+
+void Server::handleBreak(int sig) {
+    if (sig == SIGINT || sig == SIGQUIT || sig == SIGTSTP)
+        Server::interrupt = true;
+}
+
+void Server::handlingSignal() {
+    struct sigaction sig;
+
+    sig.sa_handler = &Server::handleBreak;
+    sigemptyset(&sig.sa_mask);
+    sig.sa_flags = 0;
+    if (sigaction(SIGINT, &sig, NULL) != 0)
+        displayErrorMessage("SIGINT called.");
+    if (sigaction(SIGQUIT, &sig, NULL) != 0)
+        displayErrorMessage("SIGQUIT called.");
+    if (sigaction(SIGTSTP, &sig, NULL) != 0)
+        displayErrorMessage("SIGTSTP called.");
 }
