636236f1ae43a3738020fcab081fcf14573b5b9b
Mehdi Sapin <mehdi.sapin@gmail.com>
2024-01-17 16:13:44 +0100

    Wip: commands classes function setter and getter

--- sources/commands/Commands.cpp
+++ sources/commands/Commands.cpp
@@ -6,7 +6,7 @@
 /*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 12:42:52 by msapin            #+#    #+#             */
-/*   Updated: 2024/01/16 18:15:56 by msapin           ###   ########.fr       */
+/*   Updated: 2024/01/17 16:05:49 by msapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,37 @@
 void	executeJoin(Commands & command);
 void	executeNick(Commands & command);
 void	executeUser(Commands & command);
+void	executePing(Commands & command);
 
-Commands::Commands(std::string line) {
+Commands::Commands(std::string line, Client * ptr) : _clientPtr(ptr) {
 
-    (void)line;
-    std::cout << "Create Commands " << line << std::endl;
+	std::cout << "Create Commands " << line << " " << this->_clientPtr->getNickname() << std::endl;
+
+	std::stringstream streamLine(line);
+	std::string word;
+
+	streamLine >> this->_name;
+	while (streamLine >> word)
+	{
+		this->_argSplit.push_back(word);
+	}
+}
+
+Commands::~Commands(void) {
+
+	// std::cout << "Delete Commands " << std::endl;
 }
 
 void Commands::executeCommand() {
 
-	std::string arrayCommand[] = {"NICK", "USER", "JOIN"};
-	void (*arrayFunction[4])(Commands &) = {executeNick, executeUser, executeJoin};
-    (void)arrayFunction;
+	std::string arrayCommand[] = {"NICK", "USER", "JOIN", "PING"};
+	void (*arrayFunction[5])(Commands &) = {executeNick, executeUser, executeJoin, executePing};
 
-	// std::cout << "LINE: " << line << std::endl;
-
-	// std::stringstream streamLine(line);
-	// std::string word;
-	// std::vector<std::string> lineSplit;
-
-	// while (streamLine >> word)
-	// {
-	// 	lineSplit.push_back(word);
-	// }
-
-	// for (int i = 0; i <= 1; i++)
-	// {
-	// 	if (arrayCommand[i] == *lineSplit.begin())
-	// 	{
-	// 		(*arrayFunction[i])(lineSplit, fd);
-	// 	}
-	// }
+	for (int i = 0; i <= 1; i++)
+	{
+		if (arrayCommand[i] == this->_name)
+		{
+			(*arrayFunction[i])(*this);
+		}
+	}
 }
