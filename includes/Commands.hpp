80a0e41a6d0853a7f9e6b00b11fab6c9d6839896
Mehdi Sapin <mehdi.sapin@gmail.com>
2024-01-22 17:55:06 +0100

    Use: displayError to handle different case

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -6,7 +6,7 @@
 /*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 12:43:15 by msapin            #+#    #+#             */
-/*   Updated: 2024/01/18 17:12:44 by msapin           ###   ########.fr       */
+/*   Updated: 2024/01/22 17:34:03 by msapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 # define COMMANDS_HPP
 # include "Server.hpp"
 # include "Client.hpp"
+# define ERR_NOTREGISTERED 451
+# define ERR_NEEDMOREPARAMS 461
+# define ERR_ALREADYREGISTERED 462
+# define ERR_PASSWDMISMATCH 464
 
 class Commands {
 
@@ -38,5 +42,6 @@ class Commands {
 
 void	login(Commands & command);
 void	sendMessage(int clientFd, std::string msg);
+void    displayError(int errorCode, Commands & command);
 
 #endif
