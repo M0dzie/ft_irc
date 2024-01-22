73f0a891b022be375d9bcfb6f1b7df03fd8edaa9
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-22 15:22:27 +0100

    Init: overloaded constructor and member attribute name with getter and setter

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:03:03 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/18 14:05:45 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/22 15:21:50 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,17 @@
 
 class Channel {
 private:
+    Channel();
+
+    std::string _name;
+    
 public:
+    Channel(std::string const &name) : _name(name) {}
+    ~Channel();
+
+    void setName(std::string const &name) {this->_name = name;}
+
+    std::string const &getName() const {return this->_name;}
 };
 
 #endif
