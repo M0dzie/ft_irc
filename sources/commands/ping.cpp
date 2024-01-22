44849dc7819b59f0cff00083747932f48c6909a2
Mehdi Sapin <mehdi.sapin@gmail.com>
2024-01-22 15:30:12 +0100

    Fix: missing part send to client to trigger ping pong correctly

--- sources/commands/ping.cpp
+++ sources/commands/ping.cpp
@@ -6,7 +6,7 @@
 /*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 16:03:54 by msapin            #+#    #+#             */
-/*   Updated: 2024/01/18 18:47:08 by msapin           ###   ########.fr       */
+/*   Updated: 2024/01/22 15:29:34 by msapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	executePing(Commands & command) {
 
 	std::string tmpPing = *command.getArgSplit().begin();
-	std::string serverName = "ft_irc"; /////////////////////// TEMPORARY
+	Client & tmpClient = command.getClient();
+	std::string serverMessage = ":" + tmpClient.getNickname() + "!" + tmpClient.getUsername() + "@localhost" + " PONG " + tmpPing;
 
-	sendMessage(command.getClient().getFD(), serverName + " PONG " + tmpPing);
-	// sendMessage(command.getClient().getFD(), serverName + " PONG " + tmpPing);
-	// displayMessage(SERVER, serverName + " PONG " + tmpPing);
+	sendMessage(tmpClient.getFD(), serverMessage);
+	displayMessage(SERVER, serverMessage);
 }
