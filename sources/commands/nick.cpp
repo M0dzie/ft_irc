3f3042bc246e25ca061c9ff09b0a0544d21acd8d
Mehdi Sapin <mehdi.sapin@gmail.com>
2024-01-23 14:19:42 +0100

    Protect: nickname to be set as undefined and check if password registered before changing nickname value

--- sources/commands/nick.cpp
+++ sources/commands/nick.cpp
@@ -6,7 +6,7 @@
 /*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:15:32 by msapin            #+#    #+#             */
-/*   Updated: 2024/01/18 18:14:26 by msapin           ###   ########.fr       */
+/*   Updated: 2024/01/23 14:17:49 by msapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,30 @@ void sendMessage(int clientFd, std::string msg) {
 		displayErrorMessage("send() failed.");
 }
 
-bool isNicknameValid(std::string nickname) {
+bool isNicknameValid(std::string nickname, Commands & command) {
 
 	if (nickname[0] == '#')
 		return false;
+	else if (nickname == "undefined")
+		return displayError(ERR_CANNOTBEUNDEFINED, command), false;
 	return true;
 }
 
 void	executeNick(Commands & command) {
 
-	(void)command;
-
 	std::string nickname = *(command.getArgSplit().begin());
 	std::string oldNickname = command.getClient().getNickname();
 
-	if (isNicknameValid(nickname))
+	if (!command.getClient().getPassValidate())
+		displayError(ERR_NOTREGISTERED, command);
+	else if (isNicknameValid(nickname, command))
 	{
 		command.getClient().setNickname(nickname);
 		// check if not first use of nick function
-		if (!oldNickname.empty())
+		if (oldNickname != "undefined")
 		{
 			sendMessage(command.getClient().getFD(), ":" + oldNickname + " NICK " + nickname);
 			displayMessage(SERVER, ":" + oldNickname + " NICK " + nickname);
 		}
-		// check if nick trigger login
-		// login(command);
 	}
 }
