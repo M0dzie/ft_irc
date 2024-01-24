d45235896432b58d2b84179703be73d4a7392c68
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-24 16:02:24 +0100

    Add: function to check if a client is already in a channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:03:03 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/22 17:14:14 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/24 15:49:03 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,14 @@ private:
 
     std::string _name;
     std::string _password;
+    std::vector<std::string> _clientIn;
     
 public:
     Channel(std::string const &name, std::string const &password) : _name(name), _password(password) {}
     ~Channel() {}
+
+    void updateClientIn(std::string const &name) {this->_clientIn.push_back(name);}
+    bool isAlreadyIn(std::string const &name);
 
     void setName(std::string const &name) {this->_name = name;}
     void setPassword(std::string const &password) {this->_password = password;}
