82ffc03dcdb400ee8c8cfa4beaea575505cb098c
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-01-24 16:22:08 +0100

    Add: channelLimit to check if a channel is full

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:03:03 by thmeyer           #+#    #+#             */
-/*   Updated: 2024/01/24 15:49:03 by thmeyer          ###   ########.fr       */
+/*   Updated: 2024/01/24 16:21:44 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ private:
     std::string _name;
     std::string _password;
     std::vector<std::string> _clientIn;
+    int _channelLimit;
     
 public:
     Channel(std::string const &name, std::string const &password) : _name(name), _password(password) {}
@@ -30,9 +31,11 @@ public:
     void updateClientIn(std::string const &name) {this->_clientIn.push_back(name);}
     bool isAlreadyIn(std::string const &name);
 
+    void setChannelLimit(int const &limit) {this->_channelLimit = limit;}
     void setName(std::string const &name) {this->_name = name;}
     void setPassword(std::string const &password) {this->_password = password;}
 
+    int const &getChannelLimit() const {return this->_channelLimit;}
     std::string const &getName() const {return this->_name;}
     std::string const &getPassword() const {return this->_password;}
 };
