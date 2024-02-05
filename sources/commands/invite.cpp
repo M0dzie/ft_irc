b290252eb8a17186664764d29b39f97a0966e725
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-02-05 18:21:33 +0100

    Add: error if the target didnt exist

--- sources/commands/invite.cpp
+++ sources/commands/invite.cpp
@@ -3,15 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   invite.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:11:00 by msapin            #+#    #+#             */
-/*   Updated: 2024/02/01 17:10:00 by msapin           ###   ########.fr       */
+/*   Updated: 2024/02/05 18:21:07 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/Commands.hpp"
 
 void	executeInvite(Commands & command) {
-	(void)command;
+	if (!command.getClient().getRegister())
+		return;
+	else if (command.getArgSplit().size() < 1)
+    	return (displayError(ERR_NEEDMOREPARAMS, command));
+
+	Client &client = command.getClient();
+	Client &target = foundClient(command, command.getArgSplit()[0]);
+	Channel *channel = command.getServer().getChannelList()[command.getArgSplit()[1]];
+	if (!channel) {
+		std::cout << PURPLE << BOLD << "Warning: " << RESET << client.getUsername() << " " << command.getArgSplit()[1] << " :No such channel" << std::endl;
+		return;
+	} else if (client == target) {
+		std::cout << PURPLE << BOLD << "Warning: " << RESET << client.getUsername() << " " << command.getArgSplit()[0] << " :didn't exist" << std::endl;
+		return;
+	} else if (!channel->isAlreadyIn(client.getNickname()))
+		return (displayErrorChannel(ERR_NOTONCHANNEL, client, *channel));
+	else if (!channel->getClients()[&client] || (channel->getInviteOnly() && !channel->getClients()[&client]))
+		return (displayErrorChannel(ERR_CHANOPRIVSNEEDED, client, *channel));
+	else if (channel->isAlreadyIn(target.getNickname()))
+		return (displayErrorChannel(ERR_USERONCHANNEL, client, *channel));
 }
