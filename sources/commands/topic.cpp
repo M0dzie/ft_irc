559f18126f94938705eceb95ff7938c3c87af435
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-02-05 14:01:31 +0100

    Add: protection to executeTopic

--- sources/commands/topic.cpp
+++ sources/commands/topic.cpp
@@ -3,15 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   topic.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:11:19 by msapin            #+#    #+#             */
-/*   Updated: 2024/02/01 17:10:13 by msapin           ###   ########.fr       */
+/*   Updated: 2024/02/05 14:00:01 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/Commands.hpp"
 
 void	executeTopic(Commands & command) {
-	(void)command;
+	if (!command.getClient().getRegister())
+		return;
+	else if (command.getArgSplit().size() < 1)
+    	return (displayError(ERR_NEEDMOREPARAMS, command));
+	else if (command.getArgSplit()[0][0] != '#') {
+		std::cout << "ft_irc topic" << std::endl;
+		return;
+	}
+
+	Client &client = command.getClient();
+	Channel *channel = command.getServer().getChannelList()[command.getArgSplit()[0]];
+	if (!channel->isAlreadyIn(client.getNickname()))
+		return (displayErrorChannel(ERR_NOTONCHANNEL, client, *channel));
+	else if (!channel->getClients()[&client])
+		return (displayErrorChannel(ERR_CHANOPRIVSNEEDED, client, *channel));
+	
+	if (channel->getTopic().empty())
+		displayRPL(RPL_NOTOPIC, client, *channel);
+	else
+		displayRPL(RPL_TOPIC, client, *channel);
 }
